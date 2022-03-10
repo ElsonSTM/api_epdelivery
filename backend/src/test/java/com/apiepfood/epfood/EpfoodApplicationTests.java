a13a3d0923149d1781401c6db0669a24fc686156
Elson Pinheiro <elson.silva@ufopa.edu.br>
2022-03-10 00:32:10 -0300

    Project Start

---
+++ backend/src/test/java/com/apiepfood/epfood/EpfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiepfood.epfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
