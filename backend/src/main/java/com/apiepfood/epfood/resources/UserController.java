f7821143819700e95b204b6d2c2a67f9770bd178
Elson Pinheiro <elson.silva@ufopa.edu.br>
2022-03-20 01:23:39 -0300

    User - Service layer, component registration

--- backend/src/main/java/com/apiepfood/epfood/resources/UserController.java
+++ backend/src/main/java/com/apiepfood/epfood/resources/UserController.java
@@ -1,19 +1,34 @@
 package com.apiepfood.epfood.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.apiepfood.epfood.entities.User;
+import com.apiepfood.epfood.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
+	
+	@Autowired
+	private UserService service;
 
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Elson", "engelp@gmail.com", "93 9 9191-2234", "123456");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
