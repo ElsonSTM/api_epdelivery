b6b6470c168e67f747e31a1e526b07138e77ed9b
Elson Pinheiro <elson.silva@ufopa.edu.br>
2022-03-19 23:11:42 -0300

    POST /orders

--- backend/src/main/java/com/apiepfood/epfood/resources/OrderController.java
+++ backend/src/main/java/com/apiepfood/epfood/resources/OrderController.java
@@ -1,15 +1,18 @@
 package com.apiepfood.epfood.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.apiepfood.epfood.dto.OrderDto;
-import com.apiepfood.epfood.dto.ProductDto;
 import com.apiepfood.epfood.services.OrderService;
 
 @RestController
@@ -23,5 +26,13 @@ public class OrderController {
 	public ResponseEntity<List<OrderDto>> findAll() {
 		List<OrderDto> list = service.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@PostMapping
+	public ResponseEntity<OrderDto> insert(@RequestBody OrderDto dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{id}") 
+				.buildAndExpand(dto.getId()).toUri(); // Chamar a instancia a URI do recurso que doi criado.
+		return ResponseEntity.created(uri).body(dto);
 	}
 }
--- backend/src/main/java/com/apiepfood/epfood/services/OrderService.java
+++ backend/src/main/java/com/apiepfood/epfood/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.apiepfood.epfood.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,19 +9,37 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.apiepfood.epfood.dto.OrderDto;
+import com.apiepfood.epfood.dto.ProductDto;
 import com.apiepfood.epfood.entities.Order;
+import com.apiepfood.epfood.entities.OrderStatus;
+import com.apiepfood.epfood.entities.Product;
 import com.apiepfood.epfood.repositories.OrdertRepository;
+import com.apiepfood.epfood.repositories.ProductRepository;
 
 @Service
 public class OrderService {
 	
 	@Autowired
 	private OrdertRepository repository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 
 	@Transactional(readOnly = true) //Garantir que a transação vai abrir e fechar conexão com o banco. ReadOnly não vai fazer o block de escrita no banco.
 	public List<OrderDto> findAll()  {
 		List<Order> list = repository.findOrdersWithProducts(); //Chamada da query (busca) personalizada criado na classe OrderRepository
-		return list.stream().map(x -> new OrderDto(x)).collect(Collectors.toList());
-		
+		return list.stream().map(x -> new OrderDto(x)).collect(Collectors.toList());	
+	}
+	
+	@Transactional
+	public OrderDto insert(OrderDto dto) { //OrderDTO vai ser o retorno ; OrderDTO dto vai ser o objeto a ser salvo no banco de dados
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), 
+				Instant.now(), OrderStatus.EMPREPARACAO); //instancia dos objetos a serem salvo no banco de dados.
+		for (ProductDto p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId()); //instanciar um produto, só que ele não vai no banco de dados, tudo gerenciado pelo JPA
+			order.getProducts().add(product);
+		}
+		order = repository.save(order); //guarda uma referencia para o objeto salvo.
+		return new OrderDto(order); //retornar o objeto order convertido para dto.
 	}
 }
